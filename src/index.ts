88faa0da23dd1079671c4ca1d5f510ea54ead14e
Dimitrios Dedoussis <ddedoussis@gmail.com>
2019-11-19 00:12:51 +0000

    feat(index.ts): Export Execute Errors

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ export { default as Node, Operator, Num, Symbol, Rewritable } from './Node';
 export { default as Rule } from './Rule';
 export { default as Transformation } from './Transformation';
 export { default as Executable, Namespace, Output } from './Executable';
-export { default as Command, CommandName } from './Command';
+export { default as Command, CommandName, ExecuteError } from './Command';
 export default Algebrain;
