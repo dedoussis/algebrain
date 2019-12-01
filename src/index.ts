3ad015e5fcede05fdb870fa92a873e8b10f65dc1
Dimitrios Dedoussis <ddedoussis@gmail.com>
2019-12-01 22:30:45 +0000

    refactor(index): Expose transformations

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,8 @@ export { default as Rule } from './Rule';
 export { default as Transformation } from './Transformation';
 export { default as Executable, Namespace, Output } from './Executable';
 export { default as Command, CommandName, ExecuteError } from './Command';
+export { default as differentiation } from './transformations/differntiation';
+export { default as simplification } from './transformations/simplification';
+export { default as fibonacci } from './transformations/fibonacci';
+
 export default Algebrain;
